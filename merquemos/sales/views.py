16f2526b485b694a633d852e1528117342a8e4e1
Alejandro <dcastano@apptitud.com.co>
2017-09-27 18:26:31 -0500

    Log flag

--- merquemos/sales/views.py
+++ merquemos/sales/views.py
@@ -132,6 +132,7 @@ class RatingCreate(generics.CreateAPIView):
 
 @csrf_exempt
 def checkout(request):
+    print request.data
     order = Order.objects.get(pk=request.POST['order_id'])
     order.status = 'AC'
     order.save()
