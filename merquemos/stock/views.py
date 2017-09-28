ddea9adfd883b6f45111bd409344cc56224e9a0a
Alejandro <dcastano@apptitud.com.co>
2017-09-27 23:47:48 -0500

    Add store

--- merquemos/stock/views.py
+++ merquemos/stock/views.py
@@ -53,21 +53,24 @@ class StoreDetail(generics.RetrieveAPIView):
     serializer_class = StoreSerializer
     queryset = Store.objects.all()
 
-class CategoryList(generics.ListAPIView):
-    """Obtiene el listado de categorias de producto. Si el parámetro
+class CategoryList(RequiredParametersMixin, generics.ListAPIView):
+    """Obtiene el listado de categorias de producto, filtrados por tienda
+    obtenida desde el parámetro GET 'store_id'. Si el parámetro
     'parent_category_id' está presente, se filtrara las categorías por el 
-    id de categoría padre brindado.
+    id de categoría padre brindado. 
     """
 
     serializer_class = CategorySerializer
+    required_parameters = ['store_id']
 
     def get_queryset(self):
         queryset = Category.objects.all().filter(parent=None)
+        store = Store.objects.get(pk=self.request.query_params.get('store_id'))
         parent_category_id = self.request.query_params.get('parent_category_id')
         if parent_category_id is not None:
             queryset = Category.objects.all().filter(parent__pk=parent_category_id)
         for category in queryset:
-            if category.get_related_products().count() == 0:
+            if category.get_related_products(store=store).count() == 0:
                 queryset = queryset.exclude(pk=category.pk)
         return queryset
 
