fb4a1a244d50cb4af41ca3d02ecc73b400ac9958
Alejandro <dcastano@apptitud.com.co>
2017-10-02 16:48:08 -0500

    Refresh connect redirect URL

--- merquemos/users/adapters.py
+++ merquemos/users/adapters.py
@@ -2,8 +2,14 @@ from django.conf import settings
 from django.contrib import messages
 from django.urls import reverse
 from allauth.account.adapter import DefaultAccountAdapter
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
 class AccountAdapter(DefaultAccountAdapter):
 
     def get_email_confirmation_redirect_url(self, request):
         return reverse('webclient:login')
+
+class SocialAccountAdapter(DefaultSocialAccountAdapter):
+
+    def get_connect_redirect_url(self, request):
+        return reverse('webclient:home')
