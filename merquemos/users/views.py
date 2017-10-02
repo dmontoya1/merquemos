73ef91b7ef2354f8c07550b48a1c7248e9f470eb
Alejandro <dcastano@apptitud.com.co>
2017-10-02 10:48:50 -0500

    Hot fix

--- merquemos/users/views.py
+++ merquemos/users/views.py
@@ -55,7 +55,7 @@ class FacebookAuth(APIView):
         return super(FacebookAuth, self).dispatch(*args, **kwargs)
     
     def post(self, request):        
-        access_token = request.get('access_token', '')    
+        access_token = request.POST.get('access_token', '')    
         
         try:
             app = SocialApp.objects.get(provider="facebook")
