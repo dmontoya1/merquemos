339685e41d2662d172dfe9b81fc028021a30b9c9
Alejandro <dcastano@apptitud.com.co>
2017-10-02 16:55:00 -0500

    Hot fix

--- merquemos/users/adapters.py
+++ merquemos/users/adapters.py
@@ -13,3 +13,7 @@ class SocialAccountAdapter(DefaultSocialAccountAdapter):
 
     def get_connect_redirect_url(self, request):
         return reverse('webclient:home')
+
+    def save_user(self, request, sociallogin, form=None):
+        super(DefaultSocialAccountAdapter, self).save_user(request, sociallogin, form=form)
+        return redirect(reverse('webclient:home'))
