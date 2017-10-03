4d6ec6b93fcccc1b47cf341645fe626e5f9dfb29
Alejandro <dcastano@apptitud.com.co>
2017-10-02 22:56:47 -0500

    Hot fix

--- merquemos/webclient/urls.py
+++ merquemos/webclient/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     url(r'^logout/$', auth_views.LogoutView.as_view(), name="logout"),
     url(r'^stores/(?P<city>[\w-]+)/(?P<slug>[\w-]+)/$', StoreView.as_view(), name='store'),
     url(r'^stores/(?P<store_city>[\w-]+)/(?P<store_slug>[\w-]+)/products/(?P<slug>[\w-]+)/$', ProductView.as_view(), name='product'),
-    url(r'^policies/terms/$', PrivacyPolicyView.as_view(), name="terms"),
-    url(r'^policies/privacy/$', TermsView.as_view(), name="privacy-policy"),
+    url(r'^policies/terms/$', TermsView.as_view(), name="terms"),
+    url(r'^policies/privacy/$', PrivacyPolicyView.as_view(), name="privacy-policy"),
 ]
