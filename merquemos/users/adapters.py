7a1ea81d53aa99302a714690fe5b6858b37a1744
Alejandro <dcastano@apptitud.com.co>
2017-10-02 19:38:18 -0500

    Hot fix

--- merquemos/users/adapters.py
+++ merquemos/users/adapters.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import get_user_model
+from django.shortcuts import redirect
 from django.urls import reverse
 
-from allauth.account.adapter import DefaultAccountAdapter
+from allauth.account.adapter import DefaultAccountAdapter, get_adapter
 from allauth.account.utils import perform_login
 from allauth.socialaccount import app_settings
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
@@ -19,6 +20,16 @@ class SocialAccountAdapter(DefaultSocialAccountAdapter):
 
     def get_connect_redirect_url(self, request):
         return reverse('webclient:home')
+
+    def save_user(self, request, sociallogin, form=None):
+        u = sociallogin.user
+        u.set_unusable_password()
+        if form:
+            get_adapter().save_user(request, u, form)
+        else:
+            get_adapter().populate_username(request, u)
+        sociallogin.save(request)
+        return redirect(reverse('webclient:home'))
 
     def pre_social_login(self, request, sociallogin):
         existing_user = True
