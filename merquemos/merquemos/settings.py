8f8a3f37fa07ffde7c67d8f1c4a3c923af7008e0
Alejandro <dcastano@apptitud.com.co>
2017-10-06 17:44:31 -0500

    Fix serializer

--- merquemos/merquemos/settings.py
+++ merquemos/merquemos/settings.py
@@ -225,6 +225,6 @@ FCM_DJANGO_SETTINGS = {
 }
 
 #Centrifugo Config
-CENTRIFUGE_ADDRESS = 'http://sandbox.apptitud.com.co:8008'
+CENTRIFUGE_ADDRESS = 'http://localhost:8008'
 CENTRIFUGE_SECRET = 'f@pp#qe3b7)7uy27u094t8s1pq'
 CENTRIFUGE_TIMEOUT = 10
