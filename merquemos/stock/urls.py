0524facfab2c70693bb41111e6a69ec100ac7506
Diego Alejandro Castaño Gómez <dcastano@apptitud.com.co>
2018-05-30 10:48:03 -0500

    Fix URLS

--- merquemos/stock/urls.py
+++ merquemos/stock/urls.py
@@ -15,14 +15,12 @@ from .views import (
 
 urlpatterns = [
     url(r'^stores/', StoreList.as_view(), name="stores"),
-    url(r'^brands/', BrandList.as_view(), name="brands"),
-    url(r'^store-brands/', BrandStoreList.as_view(), name="store-brands"),
     url(r'^store/(?P<pk>[0-9]+)/$', StoreDetail.as_view(), name="store-detail"),
     url(r'^categories/$', CategoryList.as_view(), name="categories"),
     url(r'^categories/(?P<pk>\d+)/', CategoryDetail.as_view(), name="categorie"),
     url(r'^products/$', ProductList.as_view(), name="products"),
-    url(r'^inventory/$', InventoryList.as_view(), name="inventory"),
-    url(r'^products/(?P<pk>\d+)/', ProductDetail.as_view(), name="product"),
+     url(r'^products/(?P<pk>\d+)/', ProductDetail.as_view(), name="product"),
+    url(r'^inventory/$', InventoryList.as_view(), name="inventory"),   
     url(r'^brands/$', BrandList.as_view(), name="brands"),
     url(r'^brands/(?P<pk>\d+)/', BrandDetail.as_view(), name="brand"),
     url(r'^store-brands/$', BrandStoreList.as_view(), name="store-brands"),
