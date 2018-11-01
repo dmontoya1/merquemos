5be583dba9d17a99eea898b297b5488860e4d794
Daniel Felipe Montoya Ramirez <dmontoya@apptitud.com.co>
2018-11-01 10:58:51 -0500

    Updating models

--- merquemos/merquemos/settings.py
+++ merquemos/merquemos/settings.py
@@ -156,10 +156,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'merquemos',
-        'USER': 'postgres',
-        'PASSWORD': 'PkvdQWIfeNvg647',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'PkvdQWIfeNvg647',
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
     }
 }
 
