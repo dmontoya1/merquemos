26fd50df9d6d9499b110627396409916620bda30
Daniel Montoya <dmontoya_lab@gmail.com>
2019-05-18 12:13:22 -0500

    Updating settings merquemos

--- merquemos/users/admin.py
+++ merquemos/users/admin.py
@@ -2,19 +2,23 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from django.contrib.auth.models import Group
+from django.contrib.auth import admin as auth_admin
 from .models import User, Address
 
-# admin.site.unregister(Group)
 
 class AddressInline(admin.StackedInline):
     model = Address
     extra = 0
 
-class UserAdmin(admin.ModelAdmin):
+
+class UserAdmin(auth_admin.UserAdmin):
     inlines = [
         AddressInline,
     ]
-    icon = '<i class="material-icons">account_circle</i>'
+    fieldsets = auth_admin.UserAdmin.fieldsets + (("Datos personales",
+                                                   {"fields": (
+                                                       "user_type", 'phone_number', 'private_hash',)}),)
+    readonly_fields = ('private_hash',)
+
 
 admin.site.register(User, UserAdmin)
